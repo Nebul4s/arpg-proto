85f4a34d9c539267b64c5f5a1be219512a465bfe
Nebul4s <j.hiltula24@gmail.com>
2025-08-07 17:19:34 +0300

    boilerplating resource attributes

--- Source/arpg/Private/AbilitySystem/ArpgAttributeSet.cpp
+++ Source/arpg/Private/AbilitySystem/ArpgAttributeSet.cpp
@@ -2,4 +2,39 @@
 
 
 #include "AbilitySystem/ArpgAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UArpgAttributeSet::UArpgAttributeSet()
+{
+}
+
+void UArpgAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UArpgAttributeSet, Life, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UArpgAttributeSet, MaxLife, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UArpgAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UArpgAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UArpgAttributeSet::OnRep_Life(const FGameplayAttributeData& OldLife) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UArpgAttributeSet, Life, OldLife)
+}
+
+void UArpgAttributeSet::OnRep_MaxLife(const FGameplayAttributeData& OldMaxLife) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UArpgAttributeSet, MaxLife, OldMaxLife)
+}
+
+void UArpgAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UArpgAttributeSet, Mana, OldMana)
+}
+
+void UArpgAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UArpgAttributeSet, MaxMana, OldMaxMana)
+}
--- Source/arpg/Public/AbilitySystem/ArpgAttributeSet.h
+++ Source/arpg/Public/AbilitySystem/ArpgAttributeSet.h
@@ -13,5 +13,29 @@ UCLASS()
 class ARPG_API UArpgAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+public:
+	UArpgAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	//Resource Attributes Start
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Life, Category="Resource Attributes")
+	FGameplayAttributeData Life;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxLife, Category="Resource Attributes")
+	FGameplayAttributeData MaxLife;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Mana, Category="Resource Attributes")
+	FGameplayAttributeData Mana;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxMana, Category="Resource Attributes")
+	FGameplayAttributeData MaxMana;
+
+	UFUNCTION()
+	void OnRep_Life(const FGameplayAttributeData& OldLife) const;
+	UFUNCTION()
+	void OnRep_MaxLife(const FGameplayAttributeData& OldMaxLife) const;
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+	//Resource Attributes End
 };
+
+
