2dedba043480257c1121e6282c5fb88915cdf213
Nebul4s <j.hiltula24@gmail.com>
2025-08-07 15:53:38 +0300

    playerstate class

---
+++ Source/arpg/Public/Player/ArpgPlayerState.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "ArpgPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API AArpgPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AArpgPlayerState();
+};
