56907941c730ffcdf2d2391ea42357a60f8dd8ce
Nebul4s <j.hiltula24@gmail.com>
2025-08-07 13:46:44 +0300

    setting up IA & IMC for player movement

---
+++ Source/arpg/Public/Player/ArpgPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ArpgPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API AArpgPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
